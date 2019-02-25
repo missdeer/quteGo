2758a8b49f49ca86b713dacbfceaae153ca10991
Fan Yang <missdeer@dfordsoft.com>
2019-02-25 15:29:09 +0800

    (*)C++17 is not well supported by msvc2015

--- src/setting.cpp
+++ src/setting.cpp
@@ -372,8 +372,6 @@ void Setting::extract_frequent_settings ()
 static std::array<const char *, NUMBER_OF_AVAILABLE_LANGUAGES> language_codes LANGUAGE_CODES;
 static std::array<const char *, NUMBER_OF_AVAILABLE_LANGUAGES> available_languages AVAILABLE_LANGUAGES;
 
-static_assert (language_codes.size () == available_languages.size ());
-
 const QStringList Setting::getAvailableLanguages()
 {
 	QStringList list;
