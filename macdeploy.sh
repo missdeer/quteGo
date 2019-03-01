602d3f5632c209c7a341f14c85061ef389a0e103
Fan Yang <me@minidump.info>
2019-03-01 09:59:34 +0800

    (*)remove warnings from mac deploy script

--- macdeploy.sh
+++ macdeploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 otool -L src/q5go.app/Contents/MacOS/q5go
-mkdir src/q5go.app/Contents/Libs
+if [ ! -d src/q5go.app/Contents/Libs ]; then mkdir src/q5go.app/Contents/Libs; fi
 cp -R src/thirdparty/QtRAR/src/libQtRAR.*dylib src/q5go.app/Contents/Libs/
 install_name_tool -change libQtRAR.1.dylib @executable_path/../Libs/libQtRAR.1.dylib src/q5go.app/Contents/MacOS/q5go
+otool -L src/q5go.app/Contents/MacOS/q5go
 
