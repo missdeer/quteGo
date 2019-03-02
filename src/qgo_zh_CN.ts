f7565025047459f1b890fc0246a6b22504eab77d
Fan Yang <missdeer@dfordsoft.com>
2019-03-02 16:34:14 +0800

    (*)fix translation

--- src/qgo_zh_CN.ts
+++ src/qgo_zh_CN.ts
@@ -4451,11 +4451,11 @@ If the picture is not set, or unvalid, a default goban is used by qgo</source>
     </message>
     <message>
         <source>Table picture</source>
-        <translation>桌面图片</translation>
+        <translation>桌面图片：</translation>
     </message>
     <message>
         <source>Path to the table picture</source>
-        <translation>表格图片路径</translation>
+        <translation>桌面图片路径</translation>
     </message>
     <message>
         <source>Path to the table picture.
