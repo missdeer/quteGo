458cff7ddf0b4a479e7a6ef49db3357fd0b34c8b
Fan Yang <me@minidump.info>
2019-03-15 23:07:17 +0800

    (*)copy translations when deploy on mac

--- macdeploy.sh
+++ macdeploy.sh
@@ -2,6 +2,8 @@
 otool -L src/q5go.app/Contents/MacOS/q5go
 if [ ! -d src/q5go.app/Contents/Libs ]; then mkdir src/q5go.app/Contents/Libs; fi
 cp -R src/thirdparty/QtRAR/src/libQtRAR.*dylib src/q5go.app/Contents/Libs/
+if [ ! -d src/q5go.app/Contents/Resources/translations ]; then mkdir src/q5go.app/Contents/Resources/translations; fi
+cp ../src/translations/*.qm src/q5go.app/Contents/Resources/translations/
 install_name_tool -change libQtRAR.1.dylib @executable_path/../Libs/libQtRAR.1.dylib src/q5go.app/Contents/MacOS/q5go
 otool -L src/q5go.app/Contents/MacOS/q5go
 
